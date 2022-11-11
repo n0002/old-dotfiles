f903767062841c220a99f74b7b931dde0f07db90
n <777777777hz@gmail.com>
2022-11-10 20:06:56 -0500

    add keybinds

--- nvim/.config/nvim/lua/n/keybinds.lua
+++ nvim/.config/nvim/lua/n/keybinds.lua
@@ -8,5 +8,8 @@ nnoremap("<C-n>", "<cmd>NERDTree<CR>")
 nnoremap("<C-f>", "<cmd>:NERDTreeFind<CR>")
 nnoremap("<leader>n", "<cmd>:NERDTreeToggle<CR>")
 
--- Tabs
-
+--Windows 
+nnoremap("<leader>sv", "<C-w>v") -- split window vertically 
+nnoremap("<leader>sh", "<C-w>s") -- split window horizontally
+nnoremap("<leader>se", "<C-w>=") -- make split windows equal width 
+nnoremap("<leader>sx", ":close<CR>") -- close current split window
