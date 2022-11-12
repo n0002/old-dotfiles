7c9b5ee5f790b32ebcf7eb8e5f9e1c77edb7bbe7
n <777777777hz@gmail.com>
2022-11-12 16:58:34 -0500

    tweaks

--- nvim/.config/nvim/lua/n/keybinds.lua
+++ nvim/.config/nvim/lua/n/keybinds.lua
@@ -13,3 +13,10 @@ nnoremap("<leader>sv", "<C-w>v") -- split window vertically
 nnoremap("<leader>sh", "<C-w>s") -- split window horizontally
 nnoremap("<leader>se", "<C-w>=") -- make split windows equal width 
 nnoremap("<leader>sx", ":close<CR>") -- close current split window
+
+--Tabs
+nnoremap("<leader>to", ":tabnew<CR>") -- open new tab 
+nnoremap("<leader>tx", ":tabclose<CR>") -- close current tab
+nnoremap("<leader>tn", ":tabn<CR>") -- go to next tab 
+nnoremap("<leader>tp", ":tabp<CR>") -- go to previous tab
+
--- nvim/.config/nvim/lua/n/plugins.lua
+++ nvim/.config/nvim/lua/n/plugins.lua
@@ -7,14 +7,25 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'vimwiki/vimwiki'
+  
+  -- get better at vim motions
   use 'ThePrimeagen/vim-be-good'
+  
   use 'preservim/nerdtree'
   use 'neovim/nvim-lspconfig'
   use 'MunifTanjim/prettier.nvim'
   use 'nvim-lua/plenary.nvim' -- Common utilities
+  
+  -- comments
   use 'kyazdani42/nvim-web-devicons' -- File icons
-   
- use {
+  
+  use 'numToStr/Comment.nvim'
+  
+  -- fuzzy finding
+  use({"nvim-telescope/telescope-fzf-native.nvim", run = "make"})
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
+ 
+  use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
@@ -23,4 +34,13 @@ use {
   'nvim-lualine/lualine.nvim',
   requires = { 'kyazdani42/nvim-web-devicons', opt = true }
 }
+
+-- completion
+use 'hrsh7th/nvim-cmp'
+use 'hrsh7th/cmp-buffer'
+use 'hrsh7th/cmp-path'
+
+-- snippets 
+use 'L3MON4D3/LuaSnip'
+use 'saadparwaiz1/cmp_luasnip'
 end)
